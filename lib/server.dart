92f735e352ae46adc8df0a724319ed133f6cb434
ObsidianXVI <siddharth.chitikela@gmail.com>
2025-03-23 09:51:34 +0800

    working access to Firestore (Mail API v10)

--- lib/server.dart
+++ lib/server.dart
@@ -1,32 +1,66 @@
 library obsivision.apis.mail;
 
+import 'package:firedart/firedart.dart';
+import 'dart:convert';
 import 'dart:io';
 
+final firestore = Firestore.instance;
 void main(List<String> args) async {
-  print('running main');
+  Firestore.initialize('obsivision-site');
   final server = await HttpServer.bind(
     InternetAddress.anyIPv4,
     int.fromEnvironment('PORT', defaultValue: 8080),
   );
-  print('bound server, listening for requests');
   server.listen((req) async {
-    print('request received');
-    final idToken = req.headers.value('X-Serverless-Authorization');
-    if (idToken != null) {
-      print('ID token is not null');
-      final tokenParts = idToken.split(' ');
-      if (tokenParts[0].toLowerCase() == 'bearer') {
-        print(tokenParts[1]);
-        print(req.uri.pathSegments);
+    print(req.headers);
+    print(req.uri.queryParameters);
+    req.response.headers
+      ..contentType = ContentType.json
+      ..set(
+        'Access-Control-Allow-Origin',
+        req.headers.value('Origin') ?? 'http://localhost:8084',
+      )
+      ..set("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
+      ..set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    final Map<String, Object?> payload = {};
+    final String? userToken = req.headers.value('User-Token');
+
+    if (req.uri.pathSegments[0] == 'subscribe') {
+      final Map<String, Object?> body =
+          jsonDecode(await utf8.decodeStream(req));
+      print(body);
+      if (req.uri.pathSegments[1] == 'post') {
+        if (req.uri.queryParameters.containsKey('postId')) {
+          if (body.containsKey('emailAddress')) {
+            req.response.statusCode = HttpStatus.ok;
+          } else {
+            req.response.statusCode = HttpStatus.badRequest;
+          }
+        } else {
+          req.response.statusCode = HttpStatus.badRequest;
+        }
+      } else if (req.uri.pathSegments[1] == 'early-access') {
+        if (req.uri.queryParameters.containsKey('project') &&
+            req.uri.queryParameters.containsKey('accessType') &&
+            body.containsKey('emailAddress')) {
+          final ref = firestore
+              .collection('mail')
+              .document('subscriptions')
+              .collection('early-access')
+              .document(req.uri.queryParameters['project'] as String)
+              .collection(req.uri.queryParameters['accessType']!);
+          await ref.document(body['emailAddress'] as String).set({});
+          req.response.statusCode = HttpStatus.ok;
+        } else {
+          req.response.statusCode = HttpStatus.badRequest;
+        }
       }
+    } else {
+      req.response.statusCode = HttpStatus.forbidden;
     }
-    req.response
-      ..headers.contentType = ContentType.json
-      ..statusCode = HttpStatus.ok
-      ..write({
-        'payload': req.uri.pathSegments,
-      });
-    print('wrote response, closing');
+
+    req.response.write(payload);
     await req.response.close();
   });
 }
